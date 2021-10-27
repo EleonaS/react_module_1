5c983c346d89a6f0939f7911e4271436521e96ee
Elena Sysoyeva <evsysoyeva@gmail.com>
2021-10-27 23:43:55 +0300

    test -7

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@
 
 function App() {
   return (
-    <div classNamedsgdhgdft="App">
-      <header className="App-header">
+    <divdggdxfhxdg className="App">
+      <headerdgdfxhg className="App-header">
         <h1>hello</h1>
 
         <p></p>
         <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
-      </header>
-    </div>
+      </headerdgdfxhg>
+    </divdggdxfhxdg>
   );
 }
 
